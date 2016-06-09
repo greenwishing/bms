fcd83d4c4a0b117304d4bc7fa3248f0836f3a726
greenwishing <greenwishing@msn.cn>
2016-06-09 16:42:19 +0800

    NumberFormat

--- bms/core/src/main/java/cn/greenwishing/bms/utils/NumberUtils.java
+++ bms/core/src/main/java/cn/greenwishing/bms/utils/NumberUtils.java
@@ -8,17 +8,24 @@ import java.text.NumberFormat;
  */
 public class NumberUtils {
 
+    public static final NumberFormat DEFAULT_NUMBER_FORMAT = buildNumberFormat(2);
+
     public static String toString(Number number) {
-        return toString(number, 2);
+        if (number == null) return "";
+        return DEFAULT_NUMBER_FORMAT.format(number);
     }
 
     public static String toString(Number number, int digits) {
         if (number == null) return "";
+        return buildNumberFormat(digits).format(number);
+    }
+
+    private static NumberFormat buildNumberFormat(int digits) {
         NumberFormat format = NumberFormat.getNumberInstance();
         format.setMinimumFractionDigits(0);
         format.setMaximumFractionDigits(digits);
         format.setGroupingUsed(false);
         format.setRoundingMode(RoundingMode.HALF_UP);
-        return format.format(number);
+        return format;
     }
 }
