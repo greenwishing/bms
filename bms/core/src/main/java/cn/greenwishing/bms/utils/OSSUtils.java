a55860162dd1d579bdcc49857fe146b216b80569
greenwishing <greenwishing@msn.cn>
2017-05-29 19:14:48 +0800

    绑定域名 images.greenwishing.cn

--- bms/core/src/main/java/cn/greenwishing/bms/utils/OSSUtils.java
+++ bms/core/src/main/java/cn/greenwishing/bms/utils/OSSUtils.java
@@ -1,12 +1,5 @@
 package cn.greenwishing.bms.utils;
 
-import cn.greenwishing.bms.cache.ConfigurationCache;
-import cn.greenwishing.bms.cache.OSSClientFactory;
-import com.aliyun.oss.OSSClient;
-import org.joda.time.DateTime;
-
-import java.net.URL;
-
 /**
  * User: Wufan
  * Date: 2017/5/29
@@ -15,14 +8,6 @@ public class OSSUtils {
 
     public static String generateImageUrl(String key) {
         if (ValidationUtils.isEmpty(key)) return "";
-        String bucketName = ConfigurationCache.get("aliyun.oss.bucketName");
-        return generateUrl(bucketName, key);
-    }
-
-    public static String generateUrl(String bucketName, String key) {
-        DateTime dateTime = new DateTime().plusWeeks(1); // 有效期 1 周
-        OSSClient client = OSSClientFactory.getClient();
-        URL url = client.generatePresignedUrl(bucketName, key, dateTime.toDate());
-        return url.toString();
+        return "http://images.greenwishing.cn/" + key;
     }
 }
