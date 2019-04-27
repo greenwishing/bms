17c87bcd3ad622bc716146c79eac7ef8b77f65bc
Frank wu <990091185@qq.com>
2019-04-27 18:14:39 +0800

    阿里云配置

--- bms/core/src/main/java/cn/greenwishing/bms/utils/OSSUtils.java
+++ bms/core/src/main/java/cn/greenwishing/bms/utils/OSSUtils.java
@@ -1,5 +1,7 @@
 package cn.greenwishing.bms.utils;
 
+import cn.greenwishing.bms.cache.ConfigurationCache;
+
 /**
  * @author Frank wu
  * @date 2017/5/29
@@ -7,7 +9,11 @@ package cn.greenwishing.bms.utils;
 public class OSSUtils {
 
     public static String generateImageUrl(String key) {
-        if (ValidationUtils.isEmpty(key)) return "";
-        return "http://images.greenwishing.cn/" + key;
+        if (ValidationUtils.isEmpty(key)) {
+            return "";
+        }
+        String bucket = ConfigurationCache.get("aliyun.oss.bucket", "bmsimgs");
+        String endpoint = ConfigurationCache.get("aliyun.oss.endpoint", "oss-cn-shenzhen.aliyuncs.com");
+        return "http://" + bucket + "." + endpoint + "/" + key;
     }
 }
